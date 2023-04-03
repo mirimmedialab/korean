82f88aff28df129ee70ae86bb1b823643bfa1bb0
Been0701 <dlwptlwkr93@naver.com>
2023-04-03 21:08:57 +0900

    [Feat] 캘린더 구현 완료

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,53 +1,82 @@
 import styled from '@emotion/styled';
-import dayjs from 'dayjs';
+import Link from 'next/link';
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
-
+import { CgProfile } from 'react-icons/cg';
+import { Datepicker, DatepickerEvent } from '@meinefinsternis/react-horizontal-date-picker';
+import { enUS } from 'date-fns/locale';
+import Image from 'next/image';
+import Sponge from 'public/sponge.webp';
+import { borderRadius } from '@mui/system';
 export function Main() {
-  const [actualDate, setActualDate] = React.useState(new Date());
+  const [date, setDate] = React.useState<{
+    endValue: Date | null;
+    startValue: Date | null;
+    rangeDates: Date[] | null;
+  }>({
+    startValue: null,
+    endValue: null,
+    rangeDates: [],
+  });
+
+  const handleChange = (d: DatepickerEvent) => {
+    const [startValue, endValue, rangeDates] = d;
+    setDate((prev) => ({ ...prev, endValue, startValue, rangeDates }));
+  };
 
   return (
     <MainContainer>
-      <Header>
-        <Logo>Voca voca</Logo>
-        <Setting>⋯</Setting>
-      </Header>
-      <FriendList>
-        <Friend>닉</Friend>
-        <Next>&gt;</Next>
-      </FriendList>
-      <Profile></Profile>
-      <Calendar
-        locale='ko'
-        onChange={() => setActualDate(new Date())}
-        value={actualDate}
-        formatDay={(locale, date) => dayjs(date).format('DD')}
-      />
-      {/* <h1>
-        {new Date(actualDate).toLocaleDateString('ko', {
-          year: 'numeric',
-          month: 'short',
-          day: 'numeric',
-        })}
-      </h1> */}
+      <Box>
+        <Header>
+          <Logo>Voca voca</Logo>
+          <Setting>⋯</Setting>
+        </Header>
+        <FriendList>
+          <Friend>스</Friend>
+          <Next>&gt;</Next>
+        </FriendList>
+        <Profile>
+          <Image
+            src={Sponge}
+            alt='profile'
+            width={'40'}
+            height={'40'}
+            style={{ marginRight: '15px', borderRadius: '50%', objectFit: 'cover' }}
+          ></Image>{' '}
+          <Nickname>스폰지밥</Nickname>
+        </Profile>
+        <Datepicker onChange={handleChange} locale={enUS} startValue={date.startValue} endValue={date.endValue} />
+        <ButtnWrapper>
+          <Button>
+            <Link href='/page2' style={{ color: '#ffff', textDecoration: 'none', fontSize: '16px' }}>
+              Word
+            </Link>
+          </Button>
+        </ButtnWrapper>
+      </Box>
     </MainContainer>
   );
 }
 
 const MainContainer = styled.div`
   background-color: #ffff;
-  width: 360px;
+  /* min-width: 360px; */
+  /* width: 1024px; */
   height: 100vh;
-  padding: 10px;
+  margin: 0 auto;
+`;
+const Box = styled.div`
+  /* width: 360px; */
+  padding: 15px;
 `;
 const Header = styled.div`
   display: flex;
   justify-content: space-between;
 `;
 const Logo = styled.div`
-  font-size: 24px;
+  font-size: 30px;
   font-weight: 700;
+  margin-bottom: 15px;
 `;
 const Setting = styled.div`
   text-align: center;
@@ -59,13 +88,14 @@ const Setting = styled.div`
 `;
 const FriendList = styled.div`
   display: flex;
+  margin-bottom: 15px;
 `;
 const Friend = styled.div`
   text-align: center;
   line-height: 36px;
   width: 36px;
   height: 36px;
-  border: 2px solid #b6b9ff;
+  border: 2px solid #b6d1ff;
   border-radius: 50%;
   margin-right: 5px;
 `;
@@ -77,4 +107,25 @@ const Next = styled.div`
   background-color: #ececec;
   border-radius: 50%;
 `;
-const Profile = styled.div``;
+const Profile = styled.div`
+  width: 240px;
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+const Nickname = styled.span`
+  font-size: 18px;
+  font-weight: 500;
+`;
+const ButtnWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const Button = styled.button`
+  width: 100px;
+  height: 40px;
+  background-color: #b6d1ff;
+  border: none;
+  outline: none;
+  margin-top: 15px;
+`;
