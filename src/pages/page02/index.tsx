14d0b5a5961cd032c07dac944df4569508673ffe
sejung1218 <sejung1218@hanmail.net>
2023-04-03 15:29:08 +0900

    Create Page02

---
+++ src/pages/page02/index.tsx
@@ -0,0 +1,16 @@
+import {Box} from "@mui/material";
+import styled from '@emotion/styled';
+
+export default function Page02() {
+  return (
+    <PageBox>
+
+    </PageBox>
+  )
+}
+
+export const PageBox = styled(Box)`
+  width: 360px;
+  height: 700px;
+  border: 1px solid white;
+`;
