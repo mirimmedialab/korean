67336314f4ccd36f69292e24ad1127a68606f179
jooheeok <jooheeok92@gamil.com>
2023-04-04 11:28:39 +0900

    [feat] 로딩 페이지 수정

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,18 +1,16 @@
 import Image from "next/image";
-// import { Image } from "@/components/Image";
 import styled from "@emotion/styled";
 
 export const Loading = () => {
   return (
     <LoadingContainor>
-      {/* <Image src="/assets/images/example.gif" alt="loading" width={500} height={500} /> */}
       <Image
         src="https://cdn.discordapp.com/attachments/1079706164994002944/1092635106172211301/ezgif.com-gif-maker.gif"
         alt="example"
         width={300}
         height={300}
       />
-      <LoadingTitle>나만의 영단어장</LoadingTitle>
+      <LoadingTitle>나만의 영어 단어장 로딩중 ...</LoadingTitle>
     </LoadingContainor>
   );
 };
