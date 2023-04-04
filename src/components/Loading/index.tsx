47c4b9377073d8d475cc7d8ed38fa60fedf7be0c
jooheeok <jooheeok92@gamil.com>
2023-04-04 11:23:53 +0900

    [feat] 로딩 페이지 생성

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,3 +1,28 @@
-export default function Loading() {
-  return <>hello</>;
-}
+import Image from "next/image";
+// import { Image } from "@/components/Image";
+import styled from "@emotion/styled";
+
+export const Loading = () => {
+  return (
+    <LoadingContainor>
+      {/* <Image src="/assets/images/example.gif" alt="loading" width={500} height={500} /> */}
+      <Image
+        src="https://cdn.discordapp.com/attachments/1079706164994002944/1092635106172211301/ezgif.com-gif-maker.gif"
+        alt="example"
+        width={300}
+        height={300}
+      />
+      <LoadingTitle>나만의 영단어장</LoadingTitle>
+    </LoadingContainor>
+  );
+};
+
+const LoadingContainor = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const LoadingTitle = styled.p`
+  font-size: 24px;
+`;
---
+++ src/pages/loading/index.tsx
@@ -0,0 +1,5 @@
+import { Loading } from "../../components/Loading/index";
+
+export default function LearningPage() {
+  return <Loading />;
+}
